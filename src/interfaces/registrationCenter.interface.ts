5dde14a85fe7d7ef0a44c4b5d108e4cc96ec9c7d
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-04-03 00:22:09 +0700

    added controllers and modules for registration dep and center

--- src/interfaces/registrationCenter.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IRegistrationCenter {
-  _id?: string;
-  centerId: string;
-  password: string;
-  name: string;
-  location: string;
-  phoneNumber: string;
-  registrationDep: string;
-}
--- src/interfaces/registrationDep.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface IRegistrationDep {
-  _id?: string;
-  password: string;
-  name: string;
-}
