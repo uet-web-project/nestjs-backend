3b9f682144760533f01688c73945e4949bf62c48
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-04-04 16:51:49 +0700

    changed env

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
-const port = parseInt(process.env.PORT) || 4000;
+const port = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
