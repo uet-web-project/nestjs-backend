b2b0f4fa421bf5ecd8ef894215dec56be6e93da1
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-04-04 17:08:17 +0700

    finish setup heroku hosting

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ const port = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  app.setGlobalPrefix('api');
 }
 bootstrap();
