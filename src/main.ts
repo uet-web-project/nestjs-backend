52822e0f719ab045c0b28e1a719905d4b7490a4d
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-04-07 19:16:36 +0700

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './modules/app.module';
 
 const port = process.env.PORT || 4000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
   await app.listen(port);
 }
 bootstrap();
